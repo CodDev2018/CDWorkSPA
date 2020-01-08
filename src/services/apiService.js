94e6ea4f055654231b2d0be0194943ba6ba3319a
Paulo Bezerra <bezerra.paulo@gmail.com>
2020-01-08 11:20:17 -0400

    Update apiService.js

--- src/services/apiService.js
+++ src/services/apiService.js
@@ -2,12 +2,12 @@ import axios from 'axios'
 import store from '../store'
 
 const apiPublic = axios.create({
-    baseURL: 'http://localhost:3000/api',
+    baseURL: 'https://api.paulobezerra.eti.br/api',
     timeout: 10000
 })
 
 const apiProtected = axios.create({
-    baseURL: 'http://localhost:3000/api',
+    baseURL: 'https://api.paulobezerra.eti.br/api',
     timeout: 10000,
     headers: {
         'x-access-token': store.state.token
